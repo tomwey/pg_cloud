ce96ba099130bae9d98bc99b9b0f0a874281470d
Tomwey <tomwey@163.com>
2017-01-21 15:34:18 +0800

    update configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
 set :pty,  false
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.0.0-p353'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_live_nav_#{Rails.env}_session"
+Rails.application.config.session_store :cookie_store, key: "_pg_cloud_#{Rails.env}_session"
