48a2635c1549fc89bed3117ee15079627dd9c483
Tomwey <tomwey@163.com>
2017-02-06 10:22:00 +0800

    fixed devise after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,4 +40,21 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  def after_sign_in_path_for(admin_user)
+    cpanel_root_path
+  end
+  
+  def after_sign_in_path_for(member_user)
+    if member_user.account_type.blank?
+      # 还未完善资料
+      more_profile_path
+    else
+      portal_root_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_member_session_path
+  end
+  
 end
